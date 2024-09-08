be6e01718794e303a95cab9dda69131e88e2a63a
AungKhant1209 <126847970+AungKhant1209@users.noreply.github.com>
2024-09-08 17:42:45 +0700

    Delete models directory

--- models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Category = mongoose.models.category || mongoose.model("category", categorySchema);
-
-export default Category;
